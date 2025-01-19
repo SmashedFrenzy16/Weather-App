e4d6f587892e626470e0716ba30bf58f1451aac9
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2025-01-19 20:16:02 +0000

    Update weather_app_gui.py adding some code

--- GUI-Weather-App/weather_app_gui.py
+++ GUI-Weather-App/weather_app_gui.py
@@ -1,1 +1,29 @@
+from tkinter import *
+import requests as rq
+from time import strftime, gmtime
+from suntime import Sun, SunTimeException
+from sys import exit
 
+root = Tk()
+
+root.title("Weather App")
+
+root.geometry("600x400")
+
+api = "ENTER YOUR API KEY HERE"
+
+title_label = Label(root, text="Weather App", font=("Helvetica", 40, "bold"))
+title_label.pack(pady=10)
+
+blank_label = Label(root, text="")
+blank_label.pack(pady=10)
+
+city_input = Entry(root, width=100, borderwidth=5, font=("Helvetica", 20))
+
+
+
+
+
+
+
+root.mainloop()
